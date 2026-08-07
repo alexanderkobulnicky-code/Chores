96a27b30229be5ff9ce511d81e8c9aa1a679664c
alexanderkobulnicky-code <alexanderkobulnicky@hotmail.com>
2026-08-07 14:40:47 -0400

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,12 +1,76 @@
-const CACHE = "chores-v2";
-const ASSETS = ["./","./index.html","./manifest.webmanifest"];
-self.addEventListener("install", e => {
+
+const CACHE = "chores-v4";
+
+const ASSETS = [
+
+  "./",
+
+  "./index.html",
+
+  "./manifest.webmanifest"
+
+];
+
+self.addEventListener("install", event => {
+
   self.skipWaiting();
-  e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)));
+
+  event.waitUntil(
+
+    caches.open(CACHE).then(cache => cache.addAll(ASSETS))
+
+  );
+
 });
-self.addEventListener("activate", e => {
-  e.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))).then(() => self.clients.claim()));
+
+self.addEventListener("activate", event => {
+
+  event.waitUntil(
+
+    caches.keys()
+
+      .then(keys =>
+
+        Promise.all(
+
+          keys
+
+            .filter(key => key !== CACHE)
+
+            .map(key => caches.delete(key))
+
+        )
+
+      )
+
+      .then(() => self.clients.claim())
+
+  );
+
 });
-self.addEventListener("fetch", e => {
-  e.respondWith(fetch(e.request).catch(() => caches.match(e.request)));
+
+self.addEventListener("fetch", event => {
+
+  event.respondWith(
+
+    fetch(event.request)
+
+      .then(response => {
+
+        const copy = response.clone();
+
+        caches.open(CACHE).then(cache => {
+
+          cache.put(event.request, copy);
+
+        });
+
+        return response;
+
+      })
+
+      .catch(() => caches.match(event.request))
+
+  );
+
 });
